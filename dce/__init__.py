ffae372ad70f4fca4caf5c1c5b26261fad4e2a91
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-03-09 09:21:25 +0000

    Initial skeleton project just to bundle Fabber library

---
+++ dce/__init__.py
@@ -0,0 +1,13 @@
+"""
+DSC Quantiphyse plugin
+
+Author: Martin Craig <martin.craig@eng.ox.ac.uk>
+Copyright (c) 2016-2017 University of Oxford, Martin Craig
+"""
+from quantiphyse.utils import get_local_shlib
+
+from .widget import FabberDceWidget
+
+QP_MANIFEST = {
+    "fabber-libs" : [get_local_shlib("fabber_models_dce", __file__)]
+}
