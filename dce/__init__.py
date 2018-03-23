3a1d30b60813671fb211bee77e092439f07d0db8
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-03-23 09:48:16 +0000

    Need to register the PkModelling process

--- dce/__init__.py
+++ dce/__init__.py
@@ -7,8 +7,10 @@ Copyright (c) 2016-2017 University of Oxford, Martin Craig
 from quantiphyse.utils import get_local_shlib
 
 from .widgets import DceWidget, FabberDceWidget
+from .process import PkModellingProcess
 
 QP_MANIFEST = {
+    "processes" : [PkModellingProcess,],
     "widgets" : [DceWidget,],
     "fabber-libs" : [get_local_shlib("fabber_models_dce", __file__)]
 }
